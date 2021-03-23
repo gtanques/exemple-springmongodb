e9257c3ee5b44090dcbd2fd96439bde8dee2a5e9
Gustavo <saintcalm1@gmail.com>
2021-03-23 01:36:10 -0300

    Tratamento de exceptions personalizadas

--- src/main/java/com/springmongodb/exemplemongodb/services/exceptions/ResourceException.java
+++ src/main/java/com/springmongodb/exemplemongodb/services/exceptions/ResourceException.java
@@ -1,5 +1,21 @@
 package com.springmongodb.exemplemongodb.services.exceptions;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class ResourceException {
+    
+    @ExceptionHandler(NotFoundObj.class)
+    public ResponseEntity<StandardError> objectNotFound(NotFoundObj e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado",
+                e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
     
 }
--- src/main/java/com/springmongodb/exemplemongodb/services/exceptions/StandardError.java
+++ src/main/java/com/springmongodb/exemplemongodb/services/exceptions/StandardError.java
@@ -1,5 +1,21 @@
 package com.springmongodb.exemplemongodb.services.exceptions;
 
-public class StandardError {
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StandardError implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+    
+    private Long timestamp;
+    private Integer status;    
+    private String error;
+    private String message;
+    private String path;
 }
