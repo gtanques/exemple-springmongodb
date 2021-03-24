bfe2c715af75d17151e6473fb030431d03cd66ba
Gustavo <saintcalm1@gmail.com>
2021-03-24 19:45:22 -0300

    Busca simples com @query

--- src/main/java/com/springmongodb/exemplemongodb/repository/PostRepository.java
+++ src/main/java/com/springmongodb/exemplemongodb/repository/PostRepository.java
@@ -5,11 +5,15 @@ import java.util.List;
 import com.springmongodb.exemplemongodb.entities.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
 }
