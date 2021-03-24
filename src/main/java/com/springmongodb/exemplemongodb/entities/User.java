c9831f38d15db65310f20b4c188dfb51d6c9fb6c
Gustavo <saintcalm1@gmail.com>
2021-03-24 14:39:48 -0300

    referenciando post do usuario

--- src/main/java/com/springmongodb/exemplemongodb/entities/User.java
+++ src/main/java/com/springmongodb/exemplemongodb/entities/User.java
@@ -1,26 +1,38 @@
 package com.springmongodb.exemplemongodb.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Id;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Document
-public class User implements Serializable{
-    
+public class User implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     @Id
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    @Getter
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
 }
