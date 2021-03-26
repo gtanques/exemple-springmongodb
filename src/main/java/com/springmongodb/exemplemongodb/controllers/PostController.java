2fa9f57f82317320139d71a0c7cac15d5b535c55
Gustavo <saintcalm1@gmail.com>
2021-03-25 23:21:35 -0300

    Consulta com varios criterios

--- src/main/java/com/springmongodb/exemplemongodb/controllers/PostController.java
+++ src/main/java/com/springmongodb/exemplemongodb/controllers/PostController.java
@@ -1,5 +1,6 @@
 package com.springmongodb.exemplemongodb.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import com.springmongodb.exemplemongodb.controllers.util.URL;
@@ -21,16 +22,27 @@ public class PostController {
     @Autowired
     private PostService service;
 
-    @GetMapping(value="/{id}" )
-    public ResponseEntity<Post> findById(@PathVariable String id){
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping(value="/titlesearch" )
-    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text){
-        text = URL.decodeParam(text);        
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+            @RequestParam(value = "minDate", defaultValue = "") String minDate,
+            @RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+        text = URL.decodeParam(text);
+        Date minimo = URL.convertDate(minDate, new Date(0L));
+        Date maximo = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, minimo, maximo);
         return ResponseEntity.ok().body(list);
     }
 }
